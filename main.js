91f537718ecd8481a999ed677c79f8d3707af531
Renner Costa <you@example.com>
2022-04-03 13:32:06 -0300

    todo-list-criado

---
+++ main.js
@@ -0,0 +1,51 @@
+const ListContent = document.getElementById('list-content');
+const FieldInput = document.getElementById('field_input');
+
+
+FieldInput.addEventListener('keypress', (e) => {
+    if (e.keyCode === 13) {
+        e.preventDefault()
+
+        let itemList = document.createElement('li');
+        let itemTask = document.createElement('p');
+        let buttonCheck = document.createElement('button');
+        let buttonDelete = document.createElement('button');
+        let containerBtns = document.createElement('div');
+
+        itemTask.innerHTML = FieldInput.value;
+        buttonCheck.innerHTML = '<i class="fas fa-check"></i>';
+        buttonDelete.innerHTML = '<i class="fas fa-trash"></i>';
+
+        containerBtns.appendChild(buttonCheck);
+        containerBtns.appendChild(buttonDelete);
+
+        itemList.appendChild(itemTask);
+        itemList.appendChild(containerBtns);
+
+        ListContent.appendChild(itemList);
+
+        buttonCheck.addEventListener('click', checkTask);
+        buttonDelete.addEventListener('click', deleteTask);
+
+        FieldInput.value = '';
+
+        function checkTask() {
+            itemTask.parentElement
+            itemTask.classList.toggle('textChecked')
+        }
+
+        function deleteTask() {
+            itemTask.parentElement
+            itemList.remove();
+        }
+    }
+
+
+   
+
+    console.log(ListContent.clientHeight);
+})
+
+
+
+
